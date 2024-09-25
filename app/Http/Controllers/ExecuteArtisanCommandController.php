4ec54ebd2675b18b1181f7eb46dd0d79133af66f
abdelhak el ahrach <winston.abdelhak@gmail.com>
2024-09-25 22:12:42 +0100

    run db

--- app/Http/Controllers/ExecuteArtisanCommandController.php
+++ app/Http/Controllers/ExecuteArtisanCommandController.php
@@ -13,23 +13,21 @@ class ExecuteArtisanCommandController extends Controller
     public function __invoke(Request $request)
     {
         // Check for a specific parameter to trigger migrations
-        if ($request->input('run_migrations')) {
-            try {
-                // Run the migrations
-                Artisan::call('migrate', [
-                    '--force' => true, // Ensure that migrations run in production
-                ]);
+        try {
+            // Run the migrations
+            Artisan::call('migrate', [
+                '--force' => true, // Ensure that migrations run in production
+            ]);
 
-                return response()->json([
-                    'message' => 'Migrations run successfully.',
-                    'output' => Artisan::output(),
-                ]);
-            } catch (\Exception $e) {
-                return response()->json([
-                    'message' => 'Error running migrations.',
-                    'error' => $e->getMessage(),
-                ], 500);
-            }
+            return response()->json([
+                'message' => 'Migrations run successfully.',
+                'output' => Artisan::output(),
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error running migrations.',
+                'error' => $e->getMessage(),
+            ], 500);
         }
 
         return response()->json(['message' => 'No action taken.']);
