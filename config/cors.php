61ec7d4e7db57a0bbb7d430859e78a7e36033818
abdelhak el ahrach <winston.abdelhak@gmail.com>
2024-09-29 19:13:03 +0100

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,19 +1,7 @@
 <?php
 
-return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+return [
 
     'paths' => ['*'],
 
@@ -21,18 +9,8 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['*'],
-
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['*'],
-
-    'max_age' => 0,
-
     'supports_credentials' => true,
-
-    'Access-Control-Allow-Credentials' => true,
-
-    'Access-Control-Allow-Origin' => ['*'],
 
 ];
