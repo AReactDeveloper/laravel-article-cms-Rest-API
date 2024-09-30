eb2fe39c3b50a5bec427cabaa01e5ba16c1ab028
abdelhak el ahrach <winston.abdelhak@gmail.com>
2024-09-30 23:34:40 +0100

    all

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,9 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(CorsMiddleware::class);
         $middleware->statefulApi();
-        //$middleware->api(prepend: [
-        //    \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        //]);
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         // Custom exception handling can be added here
