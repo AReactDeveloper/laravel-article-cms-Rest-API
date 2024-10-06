fbf75ec82aa2354bba5a3a516b87ecc6ae9cc3d6
abdelhak el ahrach <winston.abdelhak@gmail.com>
2024-10-06 15:23:22 +0100

    tr

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+
+        // Handle OPTIONS requests
+        if ($request->isMethod('OPTIONS')) {
+            return response('', 200, $response->headers->all());
+        }
+
+        return $response;
+    }
+}
