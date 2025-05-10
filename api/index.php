728d42680379a0419d6018a721aaeb183a22a8b9
abdelhak elahrach <elahracheabdelhak@gmail.com>
2025-05-11 00:20:58 +0100

    ready

--- api/index.php
+++ api/index.php
@@ -1,21 +1,17 @@
 <?php
 
-//IF $_SERVER['DOCUMENT_ROOT'] does not contain the public folder, add it
-if (strpos($_SERVER['DOCUMENT_ROOT'], "/public") === false) {
-    $_SERVER['SCRIPT_NAME'] = '/index.php';
-    $_SERVER['DOCUMENT_ROOT'] = $_SERVER['DOCUMENT_ROOT'] . "/public";
-    $_SERVER['SCRIPT_FILENAME'] = $_SERVER['DOCUMENT_ROOT'] . "/index.php";
+use Illuminate\Http\Request;
 
-    //get $_SERVER['REQUEST_URI']; until the first ? or # character
-    $_SERVER['PATH_INFO'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $_SERVER['PHP_SELF'] = "/index.php" . $_SERVER['PATH_INFO'];
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
-$vercelURL = getenv('VERCEL_URL');
 
-putenv("APP_URL=https://$vercelURL");
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-// Forward Vercel requests to public index.
-require __DIR__ . "/" . "../public/index.php";
-
-
-// put in api/index.php
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 
-Route::get('/test-db', function () {
-    try {
-        DB::connection()->getPdo();
-        return "Database connection successful!";
-    } catch (\Exception $e) {
-        return "Database connection failed: " . $e->getMessage();
-    }
-});
-
-
-Route::get('/', function (Request  $request) {
-    $origin = $request->headers->get('Origin');
-
-    return $origin;
+Route::get('/', function () {
+    return 'hello world';
 });
