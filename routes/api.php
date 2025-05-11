69fb4036ee9f8a49a0cb9b41930a35cfe998a32c
abdelhak elahrach <elahracheabdelhak@gmail.com>
2025-05-12 00:07:51 +0100

    test

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 
 // Public routes
-Route::get('/sanctum/csrf-cookie', function () {
-    return response()->json(['message' => 'CSRF cookie set']);
-});
+
 
 Route::post('login', [AuthController::class, 'login']);
 
