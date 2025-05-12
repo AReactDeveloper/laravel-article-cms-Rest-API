12e1ad5f29e5a2554901092f57956f788af23666
abdelhak elahrach <elahracheabdelhak@gmail.com>
2025-05-12 22:12:15 +0100

    deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         // Custom exception handling can be added here
