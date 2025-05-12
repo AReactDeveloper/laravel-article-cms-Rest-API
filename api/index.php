ab47aac051f63deb69efc24d79386d43cf586278
abdelhak elahrach <elahracheabdelhak@gmail.com>
2025-05-12 22:20:40 +0100

    deploy

--- api/index.php
+++ api/index.php
@@ -1,15 +1,8 @@
 <?php
 
 // Absolute path to Laravel's bootstrap/app.php
+require __DIR__ . '/../vendor/autoload.php';
+
 require __DIR__ . '/../bootstrap/app.php';
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$request = Illuminate\Http\Request::capture();
-$response = $kernel->handle($request);
-
-$response->send();
-
-$kernel->terminate($request, $response);
