c75f3f1e078c0bf78465c5cb6b418365c4c3339e
abdelhak elahrach <elahracheabdelhak@gmail.com>
2025-05-14 00:51:48 +0100

    edited

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:3000'], // Next.js frontend URL
+    'allowed_origins' => ['*'], // Next.js frontend URL
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Content-Type', 'X-CSRF-TOKEN', 'Authorization'],
     'exposed_headers' => [],
