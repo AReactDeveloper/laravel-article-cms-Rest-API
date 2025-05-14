e1db2b96d9c3fd0588b70f1a156cb23ff4f644b9
abdelhak elahrach <elahracheabdelhak@gmail.com>
2025-05-14 01:43:56 +0100

    api sanc

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,7 @@ use App\Http\Controllers\SiteInfoController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/sanctum/csrf-cookie', function () {
-    return response()->json(['message' => 'CSRF token set']);
-});
+
 
 Route::post('login', [AuthController::class, 'login']);
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/sanctum/csrf-cookie', function () {
+    return response()->json(['message' => 'CSRF token set']);
+});
+
 Route::get('/', function () {
     return 'hello world';
 });
